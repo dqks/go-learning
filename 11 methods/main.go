0ff386dcbb3d7d2b38c8b1a9f570f05ea797c40c
Maxim Shtin <maksimshtin@gmail.com>
2026-08-25 18:42:48 +0500

    methods and pointers in methods

--- 11 methods/main.go
+++ 11 methods/main.go
@@ -25,13 +25,33 @@ type user struct {
 	age                             uint
 }
 
+// Испольщуем указатель по причине описанной ниже
+func (u *user) getFullName() string {
+	return u.lastName + " " + u.firstName + " " + u.patronymic
+}
+
+// Когда мы в методе структуры мы принимаем указатель на структуру
+// При вызовые Go сам подставляет адрес
+// Тут IDE жалуется на то, что есть метод с user а есть метод *user
+// Если есть хотя бы метод *user, то стоит для всех методов и дальше прописывать *user
+// Если даже это нигде не используется
+func (u *user) setAge(age uint) {
+	u.age = age
+}
+
 func main() {
+	// Метод для псевдонима
 	namesSlice := names{"Ivan", "Sergey", "Alex"}
 	namesSlice.print()
 	fmt.Println()
 	namesSlice.add("Vanya")
 	namesSlice.print()
+	fmt.Println()
 
-	//
-
+	// Метод для структуры
+	user := user{firstName: "Максим", lastName: "Штин", patronymic: "Олегович"}
+	fmt.Println(user.getFullName())
+	fmt.Println(user.age) // 0
+	user.setAge(20)
+	fmt.Println(user.age) // 20
 }
