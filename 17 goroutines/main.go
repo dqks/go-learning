cc9b8428b1a729a0e767f9ffc3b047db33026622
Maxim Shtin <maksimshtin@gmail.com>
2026-09-01 21:36:40 +0500

    learning: almost done gorutines

--- 17 goroutines/main.go
+++ 17 goroutines/main.go
@@ -100,20 +100,186 @@ func main() {
 
 	// При этом если одновременное в буфер будет отправлено больше значений, чем емкость
 	// То будет блокировка горутины
+
+	// -------------------------------------------------------------------
+	// Канал можно закрыть
+	fmt.Println()
+	fmt.Println()
+	channelToClose := make(chan int)
+	go getNumber(channelToClose)
+	fmt.Println(<-channelToClose)
+	// При повторной отправке данных мы получим значение типа по умолчанию
+	fmt.Println(<-channelToClose)
+
+	// При чтении данных можно получать не только значение, но и то
+	// Закрытый ли канал или нет
+	channelToClose2 := make(chan int)
+	go getNumber(channelToClose2)
+	val, ok := <-channelToClose2
+	if ok {
+		fmt.Println(val)
+	} else {
+		fmt.Println("Канал закрыт")
+	}
+	val1, ok1 := <-channelToClose2
+	if ok1 {
+		fmt.Println(val1)
+	} else {
+		fmt.Println("Канал закрыт")
+	}
+
+	// -------------------------------------------------------------------
+	// Есть каналы также только для чтения или только для отправки
+
+	// Канал только для чтения
+	//var channelToSend <-chan int = make(<-chan int)
+	// Канал только для отправки
+	//var channelToRead chan<- int = make(chan<- int)
+
+	// Двунаправленный канал можно использовать в функциях как однонаправленный канал (либо для чтения либо для отправки)
+	fmt.Println()
+	// Ниже будем применять передавать двунаправленный канал как канал только для отправки
+	dualChannel := make(chan int)
+	go getNumberNoClose(dualChannel)
+	fmt.Println(<-dualChannel, "получение данных из канала только для отправки")
+
+	// Ниже отправим канал только для чтения
+	fmt.Println()
+	go printChannelNumber(dualChannel)
+	dualChannel <- 10000
+
+	fmt.Println()
+
+	// -------------------------------------------------------------------
+	// Канал можно использовать для синхронизации данных
+	// Результат передаваемый в канал необязательно должен использоваться в горутинах
+	fmt.Println()
+	fmt.Println()
+	results := make(map[int]int)
+	ch := make(chan struct{})
+	// Передаем канал на закрытие а также мап для записи в него результат
+	go getHashTable(10, ch, results)
+	// Блокируем горутину, ожидая результат канала
+	<-ch
+
+	fmt.Println(results)
+	// Хотя в примере выше лучше использовать WaitGroup
+	// Поскольку именно для этого он и создан
+
+	// -------------------------------------------------------------------
+	// select нужен для выполнения операций над готовым каналов из списка каналов (который стал готовым быстрее)
+	// он выполняется одномоментно - 1 select = 1 горутин, даже если в select несколько case
+	// default добавляют на случай, если не нужна блокировка фукнционала
+	// default не будет блокировать выполнение горутины, в которой он объявлен
+	// Если ни одна горутина не готова - выполняется default
+	doubleCh := make(chan int)
+	squareCh := make(chan int)
+	cubeCh := make(chan int)
+
+	go square(squareCh)
+	go double(doubleCh)
+	go cube(cubeCh)
+
+	squareCh <- 10
+	doubleCh <- 2
+	cubeCh <- 3
+
+	for i := 0; i <= 2; i++ {
+		select {
+		case squareVal := <-squareCh:
+			fmt.Println(squareVal, "squareVal")
+		case doubleVal := <-doubleCh:
+			fmt.Println(doubleVal, "doubleVal")
+		case cubeVal := <-cubeCh:
+			fmt.Println(cubeVal, "cubeVal")
+			//default:
+			//	fmt.Println("default")
+		}
+	}
+
+	// -------------------------------------------------------------------
+	// Данные из канала можно читать через цикл
+	fmt.Println()
+	fmt.Println()
+	factorialChannel := make(chan int)
+	go factorial(10, factorialChannel)
+
+	// Можно написать так
+	//for {
+	//	val, ok := <-factorialChannel
+	//	if !ok {
+	//		break
+	//	}
+	//	fmt.Println(val)
+	//}
+
+	// Но так лучше
+	for val := range factorialChannel {
+		fmt.Println(val)
+	}
+
+	// -------------------------------------------------------------------
+	// Гонка данных
+	// Происходит когда несколько горутин пытаются могут изменить
+	// Один и тот же участок памяти, от которого зависит выполнение самой горутины
+	// Для профилактики горутины используеся флаг -race при сборке или запуске программы
 }
 
-func receiveNumber(channel chan int) {
-	fmt.Println("Начало горутины")
-	// Передача 13 по каналу
-	// И функция main получает это значение и продолжает свою работу
-	channel <- 13
-	fmt.Println("Конец горутины")
+func factorial(n int, ch chan int) {
+	defer close(ch)
+	result := 1
+	for i := 1; i < n; i++ {
+		result *= i
+		ch <- result
+	}
 }
+
+//func receiveNumber(channel chan int) {
+//	fmt.Println("Начало горутины")
+//	// Передача 13 по каналу
+//	// И функция main получает это значение и продолжает свою работу
+//	channel <- 13
+//	fmt.Println("Конец горутины")
+//}
 
 func square(channel chan int) {
 	// 2. Горутина блокируется на этом моменте, ей нужно значение
 	num := <-channel
-	fmt.Println(num)
+	//fmt.Println(num)
 	// 5. По каналу передатеся 5*5
 	channel <- num * num
+}
+
+func getNumber(number chan<- int) {
+	number <- 10
+	// Закрываем канал
+	close(number)
+	// В закрытый канал мы больше не можем отправлять данные
+	// number <- 20
+}
+
+func getNumberNoClose(number chan<- int) {
+	number <- 10
+}
+
+func printChannelNumber(number <-chan int) {
+	fmt.Println(<-number)
+}
+
+func getHashTable(n int, ch chan struct{}, results map[int]int) {
+	for i := 1; i < n; i++ {
+		results[i] = i * i
+	}
+	// Закрытием горутины мы открываем канал
+	close(ch)
+}
+
+func double(n chan int) {
+	val := <-n
+	n <- val * 2
+}
+
+func cube(n chan int) {
+	val := <-n
+	n <- val * val * val
 }
